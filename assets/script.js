3bb1e2a27dccf184bafd100d026112e16e18228f
Victoria Garner <75230731+MsMischief@users.noreply.github.com>
2021-01-31 16:28:29 -0800

    Updating JS

--- assets/script.js
+++ assets/script.js
@@ -20,4 +20,55 @@ $(document).ready(function() {
     // using font awesome icon https://fontawesome.com/license
     // change description here - none
     const saveIcon = "./images/save-regular.svg"; 
+     // Get stored todos from localStorage
+  let storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
+
+  if (test) { console.log(storedPlans); }
+
+  // If plans were retrieved from localStorage, update the plan array to it
+  if (storedPlans !== null) {
+    planTextArr = storedPlans;
+  } else {
+    // this should only occur on first time the app is loaded in the browser
+    planTextArr = new Array(9);
+    planTextArr[4] = "LUNCH";
+  }
+
+  if (test) { console.log("full array of plned text",planTextArr); }
+
+  // set variable referencing planner element
+  let $plannerDiv = $('#plannerContainer');
+  // clear existing elements
+  $plannerDiv.empty();
+
+  if (test) { console.log("current time",nowHour12); }
+
+
+  // build calendar by row for fix set of hours
+  for (let hour = 9; hour <= 17; hour++) {
+    let index = hour - 9;
+    
+    // build row components
+    let $rowDiv = $('<div>');
+    $rowDiv.addClass('row');
+    $rowDiv.addClass('plannerRow');
+    $rowDiv.attr('hour-index',hour);
+  
+    // Start building Time box portion of row
+    let $col2TimeDiv = $('<div>');
+    $col2TimeDiv.addClass('col-md-2');
+  
+    // create timeBox element (contains time)
+    const $timeBoxSpn = $('<span>');
+    $timeBoxSpn.attr('class','timeBox');
+     // format hours for display
+    let displayHour = 0;
+    let ampm = "";
+    if (hour > 12) { 
+      displayHour = hour - 12;
+      ampm = "pm";
+    } else {
+      displayHour = hour;
+      ampm = "am";
+    }
     
